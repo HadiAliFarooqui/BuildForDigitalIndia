b4749103325239ddc8a6166cc95f6868a88172a0
Hadi Ali Farooqui <52156264+HadiAliFarooqui@users.noreply.github.com>
2019-12-31 21:08:28 +0530

    Add files via upload

---
+++ Scripts/GameManager2.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using AsyncIO;
+using NetMQ;
+using NetMQ.Sockets;
+
+public class GameManager2 : MonoBehaviour
+{
+    [SerializeField]
+    public string username;
+
+    public int maxMessages = 25;
+
+    public GameObject chatPanel, textObject;
+
+    public InputField chatBox;
+
+    public Color playerMessage, info;
+
+
+    [SerializeField]
+    List<Message> messageList = new List<Message>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+     
+    // Update is called once per frame
+    void Update()
+    {
+        if(chatBox.text!="")
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                SendMessageToChat(username + ": " + chatBox.text, Message.MessageType.playerMessage);
+                chatBox.text = "";
+            }
+        }
+        else
+        {
+            if (!chatBox.isFocused && Input.GetKeyDown(KeyCode.Return))
+                chatBox.ActivateInputField();
+        }
+        if(!chatBox.isFocused)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SendMessageToChat("You pressed the space!" , Message.MessageType.info);
+                Debug.Log("Space!!");
+            }
+        }
+    }
+    
+    public void SendMessageToChat(string text , Message.MessageType messageType)
+    {
+        if (messageList.Count >= maxMessages)
+        {
+            Destroy(messageList[0].textObject.gameObject);
+            messageList.Remove(messageList[0]);
+        }
+
+        Message newMessage = new Message();
+
+        newMessage.text = text;
+
+        GameObject newText = Instantiate(textObject, chatPanel.transform);
+
+        newMessage.textObject = newText.GetComponent<Text>();
+
+        newMessage.textObject.text = newMessage.text;
+        newMessage.textObject.color = MessageTypeColor(messageType);
+
+        messageList.Add(newMessage);
+
+        //HelloRequester h1 = new HelloRequester();
+        //h1.GetStringMessage(newMessage.text.ToString());
+
+    }
+
+    Color MessageTypeColor(Message.MessageType messageType)
+    {
+        Color color = info;
+
+        switch(messageType)
+        {
+            case Message.MessageType.playerMessage:
+                color = playerMessage;
+                break;
+        }
+
+        return color;
+    }
+}
+
+[System.Serializable]
+public class Message
+{
+    public string text;
+    public Text textObject;
+    public MessageType messageType;
+
+    public enum MessageType
+    {
+        playerMessage,
+        info
+    }
+}
---
+++ Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	private Animator myAnimator;
+	
+	private bool talk;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInput();
+    }
+	
+	void FixedUpdate()
+	{
+		HandleAnimations();
+	}
+	
+	private void HandleAnimations()
+	{
+		if(talk)
+		{
+			myAnimator.SetTrigger("Talking");
+		}
+	}
+	
+	private void HandleInput()
+	{
+		if(Input.GetKeyDown(KeyCode.LeftShift))
+		{
+			talk = true;
+		}	//myAnimator.Play("talk");
+	}
+	
+	
+}
